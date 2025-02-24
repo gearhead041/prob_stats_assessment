7741b100e8d9f74e223f29096a215633d573f3e8
Favour Adetoun <favouradetoun@gmail.com>
2025-02-24 16:48:31 +0100

    added plots

--- question 2.R
+++ question 2.R
@@ -11,6 +11,7 @@ patient_data <- patient_data[!is.na(patient_data$`Vas-12months`),]
 
 #get biomarker levels at inclusion
 biomarker_data <- read_xlsx('./biomarkers.xlsx')
+
 #get markers at 0 weeks
 biomarker_0weeks <- biomarker_data %>%
   filter(grepl("0weeks$", Biomarker)) %>%
@@ -23,13 +24,50 @@ biomarker_0weeks <- biomarker_0weeks[biomarker_0weeks$PatientID %in% patient_dat
 #do the same for patient_data
 patient_data <- patient_data[patient_data$PatientID %in% biomarker_0weeks$PatientID,]
 
-#extract vas at 12 months
-VAS_12Month <- c(patient_data %>% select(`Vas-12months`))
-patient_data <- patient_data %>% select(-`Vas-12months`)
 
-#remove patient id columns and non-numeric columns
-biomarker_0weeks <- biomarker_0weeks %>% select(-PatientID)
+
+#remove non-numeric column from biomarker
 biomarker_0weeks <- biomarker_0weeks %>% select(-Biomarker)
-patient_data <- patient_data %>% select(-PatientID)
 
-model <- lm(VAS_12Month~patient_data+biomarker_0weeks)
+full_data <- merge(patient_data, biomarker_0weeks, by = "PatientID")
+#remove patient id
+full_data <- full_data %>% select(-PatientID)
+#convert binary data
+full_data$`Sex (1=male, 2=female)` <- ifelse(full_data$`Sex (1=male, 2=female)` == 1, 1,0)
+full_data$`Smoker (1=yes, 2=no)` <- ifelse(full_data$`Smoker (1=yes, 2=no)` == 1, 1,0)
+
+#split into training and testing data randomly
+set.seed(41)
+training_index <- sample(1:nrow(full_data), size = 0.8 * nrow(full_data))
+
+training_set <- full_data[training_index,]
+testing_set <- full_data[-training_index,]
+
+#train model
+model <- lm(`Vas-12months`~.,training_set)
+
+#show residuals
+plot(model$residuals,
+     main="Plot of Residuals",
+     xlab="Index",
+     ylab = "Residuals")
+
+#check for normality with histogram
+hist(model$residuals, 
+     main = "Histogram of Residuals", 
+     xlab = "Residuals", 
+     col = "lightblue", 
+     border = "black", 
+     breaks = 20)  
+
+#make predictions on test set
+
+predictions <- predict(model, newdata = testing_set)
+residuals_model <- testing_set$`Vas-12months` - predictions
+
+plot(predictions, residuals_model, 
+     main = "Residuals vs. Fitted Values", 
+     xlab = "Predicted VAS 12-Month", 
+     ylab = "Residuals", 
+     pch = 20, col = "blue")
+abline(h = 0, col = "red", lwd = 2)
